facaf13683d5f2bb415d24250b588956f521f0ee
Gabriele Menichetti <Gabriele Menichetti>
2024-04-16 11:09:28 +0200

    inizio del codice di compressione RLE

---
+++ Converitore/RLE.java
@@ -0,0 +1,19 @@
+package convertitore;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RLE extends Application{
+	
+	 String griglia [][];
+	
+	public void start (Stage finestra) {
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+		
+	}
+	
+}
